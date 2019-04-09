73d69ea7158085b8ce7110a17a882b38f0534383
unknown <marius.arnautu@gmail.com>
2019-04-09 13:24:36 +0100

    convert ManageCoursePage from a class component to a function component using Hooks

---
+++ src/components/courses/ManageCoursePage.js
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import * as courseActions from '../../redux/actions/courseActions';
+import * as authorActions from '../../redux/actions/authorActions';
+import PropTypes from 'prop-types';
+
+function ManageCoursePage({ courses, authors, loadAuthors, loadCourses }) {
+  useEffect(() => {
+    if (courses.length === 0) {
+      loadCourses().catch(error => {
+        alert('Loading courses failed' + error);
+      });
+    }
+
+    if (authors.length === 0) {
+      loadAuthors().catch(error => {
+        alert('Loading authors failed' + error);
+      });
+    }
+  }, []);
+
+  return (
+    <>
+      <h2>Manage Course</h2>
+    </>
+  );
+}
+
+ManageCoursePage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  authors: PropTypes.array.isRequired,
+  loadCourses: PropTypes.func.isRequired,
+  loadAuthors: PropTypes.func.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    courses: state.courses,
+    authors: state.authors
+  };
+}
+
+const mapsDispatchToProps = {
+  loadCourses: courseActions.loadCourses,
+  loadAuthors: authorActions.loadAuthors
+};
+
+export default connect(
+  mapStateToProps,
+  mapsDispatchToProps
+)(ManageCoursePage);
